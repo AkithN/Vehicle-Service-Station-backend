f89bcfa22d95b8667864930013e0d677959adc29
Diluka Theekshana <145269627+dilukatheekshana@users.noreply.github.com>
2024-08-10 15:12:08 +0530

    backend changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '12345',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'vehicle_service_station',
   waitForConnections: true,
   connectionLimit: 10,
